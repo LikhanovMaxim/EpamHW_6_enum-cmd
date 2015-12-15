8fa66f261ba157ec40c3116ec42c56b4b3a9508d
Maxim Likhanov <LikhanovMaxim@yandex.ru>
2015-12-15 17:37:22 +0300

    improved visual perception tests

--- src/MonthTest.java
+++ src/MonthTest.java
@@ -6,43 +6,31 @@ public class MonthTest {
 
     @Test
     public void firstMonthNext() throws Exception {
-        Month month = Month.JANUARY;
-        Month nextMonth = Month.FEBRUARY;
-        assertTrue(month.next() == nextMonth);
+        assertTrue(Month.JANUARY.next() == Month.FEBRUARY);
     }
 
     @Test
     public void middleMonthNext() throws Exception {
-        Month month = Month.JUNE;
-        Month nextMonth = Month.JULY;
-        assertTrue(month.next() == nextMonth);
+        assertTrue(Month.JUNE.next() == Month.JULY);
     }
 
     @Test
     public void lastMonthNext() throws Exception {
-        Month month = Month.DECEMBER;
-        Month nextMonth = Month.JANUARY;
-        assertTrue(month.next() == nextMonth);
+        assertTrue(Month.DECEMBER.next() == Month.JANUARY);
     }
 
     @Test
     public void evenMonthCountDays() throws Exception {
-        Month evenMonth = Month.MARCH;
-        int result = 31;
-        assertTrue(result == evenMonth.countDays());
+        assertTrue(31 == Month.MARCH.countDays());
     }
 
     @Test
     public void oddMonthCountDays() throws Exception {
-        Month oddMonth = Month.NOVEMBER;
-        int result = 30;
-        assertTrue(result == oddMonth.countDays());
+        assertTrue(30 == Month.NOVEMBER.countDays());
     }
 
     @Test
     public void exceptionFebruaryCountDays() throws Exception {
-        Month evenMonth = Month.FEBRUARY;
-        int result = 28;
-        assertTrue(result == evenMonth.countDays());
+        assertTrue(28 == Month.FEBRUARY.countDays());
     }
 }
